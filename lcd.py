bed584cb1bc4e28cf259b0da56f6f4a6bf12052a
Brendan Cale <brendan.cale87@gmail.com>
2016-12-06 00:42:46 +0000

    Fleshing out obslcd class. Broken code currently

--- lcd.py
+++ lcd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from Adafruit_CharLCD import Adafruit_RGBCharLCD as rgblcd 
+import RPi.GPIO as GPIO
 from daemon import Daemon
 import requests
 import time
@@ -18,6 +19,10 @@ RPIN = 7
 GPIN = 5
 BPIN = 13
 
+KEY1PIN = 27
+KEY2PIN = 17
+KEY3PIN = 23
+KEY4PIN = 4
 
 FAST_LOOPDT = 1
 SLOW_LOOPDT = 60
@@ -33,6 +38,8 @@ MSG_PRESSURE = "Pressure:\n{}hPa"
 MSG_LUX = "Lux level:\n {}lx"
 MSG_ALTITUDE = "Altitude: ()m"
 MSG_TIMESTAMP = "Time since epoch\n{}"
+
+LATEST_OBS_URL = "http://localhost/api/latest"
 
 class ObsLCD:
     """
@@ -50,6 +57,7 @@ class ObsLCD:
     0b10000000 - Scroll
     """
 
+
     def Run(self):
         self.Setup()
 
@@ -57,47 +65,90 @@ class ObsLCD:
             self.Loop()
 
     def Setup(self):
-        self.lcd = rgblcd(RSPIN, EPIN, D4PIN, D5PIN, D6PIN, D7PIN, LCD_COLUMNS, LCD_ROWS, RPIN, GPIN, BPIN)
-        self.bltimeout = time.time()
-        self.state = 0xb00000001
+        self._lcd = rgblcd(RSPIN, EPIN, D4PIN, D5PIN, D6PIN, D7PIN, LCD_COLUMNS, LCD_ROWS, RPIN, GPIN, BPIN)
+        self._gpio = self.lcd._gpio
+        self._bltimeout = time.time()
+        self._state = 0xb00000001
         
-        self.latest = {}
+        self._latest = {}
 
         # loop vars
-        self.slowtime = time.time()
-        self.fasttime = time.time()
+        self._slowtime = time.time()
+        self._fasttime = time.time()
+
+        self._gpio.add_event_detect(KEY1PIN, GPIO.FALLING, callback=callback, bouncetime=300)
+        self._gpio.add_event_detect(KEY2PIN, GPIO.FALLING, callback=callback, bouncetime=300)
+        self._gpio.add_event_detect(KEY3PIN, GPIO.FALLING, callback=callback, bouncetime=300)
+        self._gpio.add_event_detect(KEY4PIN, GPIO.FALLING, callback=callback, bouncetime=300)
+
+        self._keys = list()
 
     def Loop(self):
         # Fast loop
-        if self.fasttime >= time.time():
-
+        if self._fasttime >= time.time():
+            # Check LCD timout
+            if self._lcdon:
+                if lcd._bltimeout > time.time():
+                    self._lcdon = False
+                    lcd.set_color(0,0,0)
             # Add the Delta time for our fast loop
-            self.fasttime += FAST_LOOPDT
+            self._fasttime += FAST_LOOPDT
 
         # Slow loop
-        if self.slowtime >= time.time():
-            if self.state == 0:
+        if self._slowtime >= time.time():
+            if self._state == 0:
                 # Update messages with the lastest weather observation data point
-                self.setMessage(self.GetLatest())
+                self.SetupMessages(self.GetLatest())
 
 
             # Add the Delta time for our slow loop
-            self.slowtime += SLOW_LOOPDT
+            self._slowtime += SLOW_LOOPDT
             
 
     def SetupMessages(self, obsPoint):
-        self.lastMessage = self.message
-        self.message = line1 + "\n" + line2
+        self._lastMessage = self._message
+        self._message = line1 + "\n" + line2
         
 
     def GetLatest(self):
-        r = requests.get("http://localhost/api/latest")
+        r = requests.get(LATEST_OBS_URL)
 
         if r.status_code == 200:
             return True, r.json() 
         else:
             return False, "API returned\nStatus code {}".format(r.status_code)
 
+    def ProcessKeys(self, keys):
+        
+        if len(keys) > 0:
+            # Update the backlight timeout 
+            if self._lcdon == False:
+                self._lcdon = True
+                self._bltimeout = time.time() + BACKLIGHT_TIMEOUT
+            else:
+                # Process each key that has been pressed
+                for key in keys:
+                    if key == KEY1PIN:
+                        pass
+                    elif key == KEY2PIN:
+                        pass
+                    elif key == KEY3PIN:
+                        pass
+                    elif key == KEY4PIN:
+                        pass
+
+    def ToggleBacklight(self):
+        if self._lcdon:
+            self._gpio.setup(RPIN, GPIO.IN)
+            self._gpio.setup(GPIN, GPIO.IN)
+            self._gpio.setup(BPIN, GPIO.IN)
+        else:
+            self._gpio.setup(RPIN, GPIO.OUT)
+            self._gpio.setup(GPIN, GPIO.OUT)
+            self._gpio.setup(BPIN, GPIO.OUT)
+
+    def Callback(self, pin):
+        self._keys.append(pin)
 
 #if __name__ == "__main__":
 #        daemon = lcd('/tmp/lcd-daemon.pid')
